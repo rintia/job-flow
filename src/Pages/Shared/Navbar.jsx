21a3fb64c02a15586044b6e46f9601a7d7ad994f
Rintia Islam <rintiaislam110@gmail.com>
2023-11-05 19:05:28 +0600

    navbar made sticky

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         .catch(error => console.log(error))
      }
       return (
-          <div className="navbar ">
+          <div className="navbar sticky top-0 z-50 bg-white">
     <div className="navbar-start">
       <div className="dropdown">
         <label tabIndex={0} className="btn btn-ghost lg:hidden">
